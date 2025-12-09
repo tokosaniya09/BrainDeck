07d7d26c8adb51de73a1ed2e1a8c1eccf5ee7ee0
Toko saniya <tokosaniya09@gmail.com>
2025-12-09 16:08:12 +0530

    Deployed backend link is added in the vite.config.ts file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://braindeck-4bis.onrender.com',
         changeOrigin: true,
         secure: false,
       }
